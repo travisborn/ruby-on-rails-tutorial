8ed73024aae8c626b4f625f47c764328e8eea13f
Travis Born <tborn9@gmail.com>
2025-12-18 12:02:26 -0700

    Initial commit and hello_app from ch 1.

---
+++ hello_app/app/controllers/application_controller.rb
@@ -0,0 +1,16 @@
+class ApplicationController < ActionController::Base
+  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  allow_browser versions: :modern
+
+  # Changes to the importmap will invalidate the etag for HTML responses
+  stale_when_importmap_changes
+
+  def hello
+    render html: "Hello, World!"
+  end
+
+  def goodbye
+    render html: "Goodbye, World!"
+  end
+
+end
